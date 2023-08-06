213df41cdffe822d794af9873a763236dc452dfd
shufol islam <shufolislam1255@gmail.com>
2023-08-06 13:51:12 +0600

    orders half done

--- src/Loader/CartProductLoader.js
+++ src/Loader/CartProductLoader.js
@@ -1,8 +1,25 @@
+import { getShoppingCart } from "../assets/utilities/fakedb";
+
 const cartProductLoader = async () => {
     const response = await fetch('/fakeData/products.json');
     const products = await response.json();
-    console.log(products);
-    return products;
+
+            const storedCart = getShoppingCart();
+        let finalCart = [];
+
+
+        // object er upor for in loop
+        for (const id in storedCart) {
+            const addedProductInLocalStorage = products.find(product => product.id === id)  //finding added product from local storage using id (product{eita hoilo upor e sob product theke find koira ekta product er id} er id === addedProductInLocalStorage er id)
+
+            if (addedProductInLocalStorage) {
+                const quantity = storedCart[id];
+                addedProductInLocalStorage.quantity = quantity;
+                finalCart.push(addedProductInLocalStorage);
+            }
+        }
+
+    return finalCart;
 }
 
 export default cartProductLoader;
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -2,15 +2,15 @@ import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
 
 const Orders = () => {
-    const products = useLoaderData();
+    const cart = useLoaderData();
     return (
         <>
             <div className="shop-container">
                 <div className="product-container">
-                    <h2>This is orders: {products.length}</h2>
+                    <h2>This is orders: {cart.length}</h2>
                 </div>
                 <div className='cart-container'>
-                    <Cart cart={[]}></Cart>
+                    <Cart cart={cart}></Cart>
                 </div>
             </div>
         </>
